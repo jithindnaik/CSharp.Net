547cd041deaec20abb099a6a6b129915c8e01148
Jithin Naik <jithindnaik@gmail.com>
2023-01-01 16:35:11 +0530

    ++ Inheritance ++Getter Setter ++Enum

--- EnumInCSharp/Program.cs
+++ EnumInCSharp/Program.cs
@@ -1,3 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    public enum Colors: byte
+    {
+        RED = 1,
+        YELLOW = 2,
+        GREEN = 3,
+        BLUE = 4,
+        ORANGE = 5
+    }
 
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(Colors.BLUE);
+
+        Console.WriteLine((int)Colors.BLUE);
+    }
+}
